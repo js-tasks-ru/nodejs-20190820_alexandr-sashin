01d94ba855d7c0b402d936464dc28228931049be
Alexandr <alexander.sashin@bostongene.com>
2019-08-28 17:38:41 +0300

    add solution for 0-module 1-task

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,5 +1,11 @@
 function sum(a, b) {
-  /* ваш код */
+  var checkIsNumberType = function(value) {
+    return typeof value === 'number';
+  };
+  if (!checkIsNumberType(a) && !checkIsNumberType(b)) throw new TypeError('both arguments are not numbers');
+  if (!checkIsNumberType(a)) throw new TypeError('the first argument is not number');
+  if (!checkIsNumberType(b)) throw new TypeError('the second argument is not number');
+  return a + b;
 }
 
 module.exports = sum;
